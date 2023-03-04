f2d06c6521f3fe51fae09aaac851d6cbc6b2a216
corty2987 <cortyblackwell@gmail.com>
2023-03-04 14:58:49 -0600

    Studio Finished

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+  
+    const container = document.getElementById("container");
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);  
+        });
+    });
+});
